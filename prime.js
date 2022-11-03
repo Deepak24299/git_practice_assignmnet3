31e4c9622682efca74b4ef3203b06af2cddc5a5a
unknown <Amanbhart>
2022-11-03 11:57:51 -0700

    prime code fixed

--- prime.js
+++ prime.js
@@ -1,7 +1,21 @@
 function checkPrime(num){
+let factor=0;
 for(let i=1; i<=num; i++){
-if(num%i){
-return true;
+if(num%i==0){
+  factor++;
+
 }
 }
-checkPrime(13);
+if(counter==2){
+return true
+}
+return false
+}
+let ans=check prime(13)
+if(ans==true){
+   console.log("Prime")
+      }else{
+ console.log("Not Prime")
+}
+
+
